6cc8a906a3b6b79e322a11cf23c79ac7f1f8cfd8
s_carleton <steve.carleton@tpicap.com>
2023-08-10 12:58:45 +0100

    Commit

--- Carleton.API/Migrations/20230810111721_CarletonInfoDBInitialMigration3.Designer.cs
+++ Carleton.API/Migrations/20230810111721_CarletonInfoDBInitialMigration3.Designer.cs
@@ -4,6 +4,7 @@ using Carleton.API.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Carleton.API.Migrations
 {
     [DbContext(typeof(CarletonInfoContext))]
-    partial class CarletonInfoContextModelSnapshot : ModelSnapshot
+    [Migration("20230810111721_CarletonInfoDBInitialMigration3")]
+    partial class CarletonInfoDBInitialMigration3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Carleton.API/Migrations/20230810111721_CarletonInfoDBInitialMigration3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Carleton.API.Migrations
+{
+    public partial class CarletonInfoDBInitialMigration3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "AuthenticationDate", "RegistrationDate" },
+                values: new object[] { new DateTime(2023, 8, 10, 12, 17, 20, 212, DateTimeKind.Local).AddTicks(50), new DateTime(2023, 8, 10, 12, 17, 20, 212, DateTimeKind.Local).AddTicks(78) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "AuthenticationDate", "RegistrationDate" },
+                values: new object[] { new DateTime(2023, 8, 6, 21, 57, 16, 611, DateTimeKind.Local).AddTicks(8752), new DateTime(2023, 8, 6, 21, 57, 16, 611, DateTimeKind.Local).AddTicks(8784) });
+        }
+    }
+}
